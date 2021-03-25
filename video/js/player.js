2701557382375ad2565024354829f83d7343aefe
Ja. Harochau <harochau@gmail.com>
2021-03-25 13:27:49 +0300

    upd

--- video/js/player.js
+++ video/js/player.js
@@ -8,19 +8,34 @@ function init() {
     volume = document.getElementById("volume");
     
     play.addEventListener("click",startPlayback)
+    bar.addEventListener("click",()=>{changeVideoTiming(event)})
+
+    mmedia.muted=true;
 }
 function startPlayback() {
     if (mmedia.paused == true || mmedia.ended == true){
     mmedia.play();
     play.value = "Пауза";
+    loop = setInterval(status,1000);
     } else {
         mmedia.pause();
         play.value = "Воспр.";
-
+        clearInterval(loop);
+    }   
+}
+function status() {
+    if (!mmedia.ended){
+    progress.style.width = `${parseInt(max*mmedia.currentTime/mmedia.duration)}px`;
+    // loop = setInterval(status,1000);
+    } else {
+        progress.style.width = `0`;
+        play.value = "Воспр."
+        clearInterval(loop);
     }
+}
+function changeVideoTiming(e){
+    console.log(e.offsetX); 
     
 
-    
-    
 }
 addEventListener("load",init);
